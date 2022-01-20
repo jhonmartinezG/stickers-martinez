b2eb163c7c9c75a039cd6979acc88a881177d7be
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2022-01-19 21:28:02 -0500

    add texts for test the application

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my app stickers martinez store
         </p>
+        <h1>This is just a title for test</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
