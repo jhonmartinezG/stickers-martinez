70e6811221fcd43e080e2fb05bb3482f1cc1c24b
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2022-01-26 23:19:51 -0500

    delete default files and add tailwind css to the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to my app stickers martinez store
-        </p>
-        <h1>This is just a title for test</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='text-3xl font-bold underline'>Test title</h1>
     </div>
   );
 }
