b37153490c0505994bec596be561d40a79273d5b
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2022-01-31 22:47:38 -0500

    fix some issues on ItemListContainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const ItemListContainer = ({ greeting }) => {
+const ItemListContainer = ({ greeting, text }) => {
     return (
-        <div className='flex w-5/5 justify-center items-center h-44'>
+        <div className='flex w-5/5 justify-center items-center h-44 flex-col'>
             <h1 className='text-6xl font-extrabold underline hover:underline decoration-sky decoration-8'>{greeting}</h1>
         </div>
     );
