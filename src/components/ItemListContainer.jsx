f79e9aa5a5c868067af667fd787bb3ae355b6d7e
Jhon Sebastian Martinez Soto <jhon.martinez@globant.com>
2022-01-31 22:48:33 -0500

    delete useless props

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const ItemListContainer = ({ greeting, text }) => {
+const ItemListContainer = ({ greeting }) => {
     return (
         <div className='flex w-5/5 justify-center items-center h-44 flex-col'>
             <h1 className='text-6xl font-extrabold underline hover:underline decoration-sky decoration-8'>{greeting}</h1>
