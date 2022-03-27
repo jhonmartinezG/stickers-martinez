8b67967d97a9908b9484a95c69b388ddb104abfa
jhonmartinezG <jhon.martinez@globant.com>
2022-03-27 17:39:30 -0500

    fix alt sugestion

--- src/components/Carousel/CarouselComponent.jsx
+++ src/components/Carousel/CarouselComponent.jsx
@@ -6,16 +6,16 @@ const CarouselComponent = () => {
   return (
     <Carousel>
         <div>
-            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-3C_1944x.jpg" alt='caorusel-image' />
+            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-3C_1944x.jpg" alt='caorusel' />
         </div>
         <div>
-            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-2C_1944x.jpg" alt='caorusel-image' />
+            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-2C_1944x.jpg" alt='caorusel' />
         </div>
         <div>
-            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-4C_1944x.gif"  alt='caorusel-image'/>
+            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-4C_1944x.gif"  alt='caorusel'/>
         </div>
         <div>
-            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-1C_1944x.gif" alt='caorusel-image' />
+            <img src="https://cdn.shopify.com/s/files/1/0318/3188/3907/files/Banner-1C_1944x.gif" alt='caorusel' />
         </div>
     </Carousel>
   )
