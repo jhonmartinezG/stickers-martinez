d8f443e057579f0fc3bb84a4e65c60d305238c67
jhonmartinezG <jhon.martinez@globant.com>
2022-04-13 11:41:13 -0500

    add picture on the QuienesSomos page

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './QuienesSomos.css'
+import picture from '../../assets/picture.jpeg';
 
 const QuienesSomos = () => {
   return (
@@ -15,7 +16,7 @@ const QuienesSomos = () => {
           This website was made for educational rasons, the images and some texts are from another websites and internet, so if something belongs to you please contact to me by email
           <a href = "mailto: jhsebastian12@gmail.com"> jhsebastian12@gmail.com</a>
         </h4>
-        <img className='image' src="https://scontent-bog1-1.xx.fbcdn.net/v/t39.30808-6/178625271_4669731599704898_4150218170658490307_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=UptaWZjNUSMAX-sUig0&_nc_ht=scontent-bog1-1.xx&oh=00_AT_uUzieGqKTREpFNmeIvnff_lcaJ3uEzon5RunsuarvkA&oe=62458830" alt="" />
+        <img className='image' src={picture} alt="myself" />
       </div>
     </div>
   )
